e99b5811aace8c5f9bdf26218dd0dec2118e2003
Iseeicy <iseeicyhawkston@gmail.com>
2023-06-14 22:07:21 -0500

    Command can be imported correctly

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Icy's SGB Mod - Naninovel Plugin
 Naninovel interactions for Icy's SGB mods
+
+
+## Setup
+
+After importing, make sure to add "BobboNet.SGB.IMod.Naninovel" to the list of Type Assemblies in the Naninovel Engine Configuration.
---
+++ Runtime/Commands/EnterSGBProject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Naninovel;
+using Naninovel.Commands;
+using BobboNet.SGB.IMod;
+
+[CommandAlias("enterSGBProject")]
+public class EnterSGBProject : Command
+{
+    [RequiredParameter, Documentation("The name of the SGB project to enter.")]
+    public StringParameter Name;
+
+    public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+    {
+        SGBManager.LoadSmileGame(Name.Value);
+        return UniTask.CompletedTask;
+    }
+}
