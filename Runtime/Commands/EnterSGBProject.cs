c1faa09f5a41e7ace13c4b149fac34bbafa3cd8a
Iseeicy <iseeicyhawkston@gmail.com>
2023-07-25 19:40:39 -0500

    FrontendModeManager: Naninovel <-> SGB Handoff (#6)
    This PR creates the class FrontendModeManager for managing the handoff of state between SGB and Naninovel. ATM, it only supports entering SGB.

--- Runtime/Commands/EnterSGBProject.cs
+++ Runtime/Commands/EnterSGBProject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Naninovel;
 using Naninovel.Commands;
 using BobboNet.SGB.IMod;
+using BobboNet.SGB.IMod.Naninovel;
 
 [CommandAlias("enterSGBProject")]
 public class EnterSGBProject : Command
@@ -13,26 +14,6 @@ public class EnterSGBProject : Command
 
     public override async UniTask ExecuteAsync(AsyncToken asyncToken = default)
     {
-        // Thanks to the NaniNovel docs for implementing this nearly verbatim
-        // (https://naninovel.com/guide/integration-options.html#switching-modes)
-
-        // 1. Disable Naninovel input.
-        var inputManager = Engine.GetService<IInputManager>();
-        inputManager.ProcessInput = false;
-
-        // 2. Stop script player.
-        var scriptPlayer = Engine.GetService<IScriptPlayer>();
-        scriptPlayer.Stop();
-
-        // 3. Reset state.
-        var stateManager = Engine.GetService<IStateManager>();
-        await stateManager.ResetStateAsync();
-
-        // 4. Kill the NaniNovel camera.
-        var naniCamera = Engine.GetService<ICameraManager>().Camera;
-        naniCamera.enabled = false;
-
-        // 5. Load SGB!
-        SGBManager.LoadSmileGame(Name.Value);
+        await FrontendModeManager.EnterSGBGame(Name.Value); // Load in to the SGB game
     }
 }
---
+++ Runtime/FrontendModeManager.cs
@@ -0,0 +1,58 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using Naninovel;
+using BobboNet.SGB.IMod;
+
+namespace BobboNet.SGB.IMod.Naninovel
+{
+    public static class FrontendModeManager
+    {
+        //
+        //  Public Methods
+        //
+
+        public static async Task EnterSGBGame(string smileGameName)
+        {
+            await SetNaninovelActive(false);            // Pause Naninovel...
+            SGBManager.LoadSmileGame(smileGameName);    // ...and start SGB
+        }
+
+        public static async Task EnterNaninovel()
+        {
+            await SetNaninovelActive(true);
+
+            // TODO - Unload smile game
+            // SGBManager.UnloadSmileGame( FILL IN );
+        }
+
+        //
+        //  Private Methods
+        //
+
+        private static async Task SetNaninovelActive(bool isActive)
+        {
+            // Thanks to the NaniNovel docs for implementing this nearly verbatim
+            // (https://naninovel.com/guide/integration-options.html#switching-modes)
+
+            // 1. Set Naninovel input active or inactive.
+            var inputManager = Engine.GetService<IInputManager>();
+            inputManager.ProcessInput = isActive;
+
+            // 2. Start or Stop script player.
+            var scriptPlayer = Engine.GetService<IScriptPlayer>();
+            if(isActive) {
+                scriptPlayer.Play();
+            } else {
+                scriptPlayer.Stop();
+            }
+
+            // 3. Reset state.
+            var stateManager = Engine.GetService<IStateManager>();
+            await stateManager.ResetStateAsync();
+
+            // 4. Set NaniNovel camera active or inactive.
+            var naniCamera = Engine.GetService<ICameraManager>().Camera;
+            naniCamera.enabled = isActive;
+        }
+    }
+}
