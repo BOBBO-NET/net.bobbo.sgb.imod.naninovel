40f97d09049c2d6740c68747efe49b1c62d3df22
LAPTOP-3BPAUQ9G\Iseei <iseeicyhawkston@gmail.com>
2023-08-09 11:23:10 -0500

    SGBIMod specific naninovel config exists

---
+++ Runtime/SGBIModConfiguration.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Naninovel;
+
+namespace BobboNet.SGB.IMod.Naninovel
+{
+    [EditInProjectSettings]
+    public class SGBIModConfiguration : Configuration
+    {
+        [Header("Debug")]
+        [Tooltip("The name of the script to return to when selecting the 'Return to Naninovel' option in the IMod Overlay. "
+                + "If empty, then this will default to the project's starting script.")]
+        public string debugOverlayReturnScriptName = "";
+    }
+}
