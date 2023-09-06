506a8a603f32821279c7750245142f5d0c15348a
Iseeicy <iseeicyhawkston@gmail.com>
2023-09-06 15:01:46 -0500

    SGBPauseMenuOptions use Config (#39)
    This PR revises the SGBIModConfiguration to let users adjust SGB's pause menu settings. Effectively this means you can now enable / disable pause menu options from the Naninovel Config.

--- Runtime/SGBIModConfiguration.cs
+++ Runtime/SGBIModConfiguration.cs
@@ -7,11 +7,36 @@ namespace BobboNet.SGB.IMod.Naninovel
     [EditInProjectSettings]
     public class SGBIModConfiguration : Configuration
     {
+        //
+        //  Classes
+        //
+
+        [System.Serializable]
+        public class SGBMenuButton
+        {
+            public bool isVisible = true;
+            public bool isInteractable = true;
+        }
+
+        //
+        //  Fields
+        //
+
         [Header("Debug")]
         [Tooltip("The name of the script to return to when selecting the 'Return to Naninovel' option in the IMod Overlay. "
                 + "If empty, then this will default to the project's starting script.")]
         [ResourcePopup(ScriptsConfiguration.DefaultPathPrefix, ScriptsConfiguration.DefaultPathPrefix, ResourcePopupAttribute.EmptyValue)]
         public string overlayReturnScriptName = "";
+
+        [Header("SGB Pause Menu")]
+        public SGBMenuButton itemsButton = new SGBMenuButton();
+        public SGBMenuButton skillsButton = new SGBMenuButton();
+        public SGBMenuButton equipmentButton = new SGBMenuButton();
+        public SGBMenuButton statusButton = new SGBMenuButton();
+        public SGBMenuButton saveButton = new SGBMenuButton();
+        public SGBMenuButton configButton = new SGBMenuButton();
+        public SGBMenuButton closeButton = new SGBMenuButton();
+        public SGBMenuButton exitButton = new SGBMenuButton();
 
         [Header("Naniscript Return Targets")]
         public List<NaniscriptReturnTarget> returnTargets = new List<NaniscriptReturnTarget>();
